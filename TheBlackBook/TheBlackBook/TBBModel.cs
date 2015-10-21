936389149bb15b12a1b7d8c95ed66f05d8dddcc3
Alex Carstensen <matthacker.alex@gmail.com>
2015-10-21 11:58:30 +0200

    Added TBBCollection Added TBBModel Properties and constructors Linked it to MainWindow

--- TheBlackBook/TheBlackBook/TBBModel.cs
+++ TheBlackBook/TheBlackBook/TBBModel.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TheBlackBook
 {
-    class TBBModel
+    public class TBBModelCollection : ObservableCollection<TBBModel>
     {
+        public TBBModelCollection()
+        {
+            Add(new TBBModel("Alex", 100));
+        }
+    }
+    public class TBBModel
+    {
+        public TBBModel() { }
+
+        public TBBModel(string name, double owe)
+        {
+            Name = name;
+            Owe = owe;
+        }
+        public string Name { get; set; }
+        public double Owe { get; set; }
     }
 }
