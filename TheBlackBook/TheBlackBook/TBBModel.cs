d4346d75fde18ef2626d09ee11bbc71ed86f4ba6
Jeba Ranganathan <jeba477@hotmail.com>
2015-10-22 12:15:50 +0200

    Desig and structure changed. Missing only total price convertetr

--- TheBlackBook/TheBlackBook/TBBModel.cs
+++ TheBlackBook/TheBlackBook/TBBModel.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using TheBlackBook.Annotations;
 
 namespace TheBlackBook
 {
@@ -11,19 +16,56 @@ namespace TheBlackBook
     {
         public TBBModelCollection()
         {
-            Add(new TBBModel("Alex", 100));
+            var locVar = new TBBModel("Alex");
+            locVar.Transaction.Add(new TBBTransaction(DateTime.Now, 100));
+            Add(locVar);
+
+            var locVar1 = new TBBModel("Jeba");
+            locVar1.Transaction.Add(new TBBTransaction(DateTime.Now.AddHours(-4), -75));
+            Add(locVar1);
         }
     }
-    public class TBBModel
+    public class TBBModel : INotifyPropertyChanged
     {
-        public TBBModel() { }
+        public TBBModel()
+        {
+            Name = "Name";
+            Transaction = new ObservableCollection<TBBTransaction>();
+            Transaction.Add(new TBBTransaction());
+            Transaction.CollectionChanged += delegate { OnPropertyChanged(nameof(Transaction)); };
+        }
 
-        public TBBModel(string name, double owe)
+        public TBBModel(string name)
         {
             Name = name;
-            Owe = owe;
+            Transaction = new ObservableCollection<TBBTransaction>();
+            Transaction.CollectionChanged += delegate {OnPropertyChanged(nameof(Transaction)); };
         }
         public string Name { get; set; }
-        public double Owe { get; set; }
+        public ObservableCollection<TBBTransaction> Transaction { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class TBBTransaction
+    {
+        public TBBTransaction()
+        {
+            TBBDateTime = DateTime.Now;
+            Transfer = 0;
+        }
+        public TBBTransaction(DateTime date, double transferAmout )
+        {
+            TBBDateTime = date;
+            Transfer = transferAmout;
+        }
+        public DateTime TBBDateTime { get; set; }
+        public double Transfer { get; set; }
     }
 }
---
+++ TheBlackBook/TheBlackBook/TotalValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace TheBlackBook
+{
+    public class TotalValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as ObservableCollection<TBBTransaction>)?.Sum(t => t.Transfer);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
